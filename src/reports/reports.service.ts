7c9c4e13d77dc8513adbb06843a0a5b8984a5c7e
wilsoncantin81 <wilsoncantin@gmail.com>
2026-04-01 21:34:24 -0500

    Update reports.service.ts

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -87,7 +87,7 @@ export class ReportsService {
     const transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST,
       port: Number(process.env.SMTP_PORT) || 465,
-      secure: true,
+      secure: false,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
